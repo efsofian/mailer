094ae8af44ffcc76c5494599c0c7eaaac379510c
efsofian <ef.sofian@gmail.com>
2022-01-06 16:56:24 +0100

    changes

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(
-		`currently connected in ${process.env.NODE_ENV} MODE on PORT:${PORT}`
+		`currently connected in ${process.env.NODE_ENV} mode on PORT:${PORT}`
 	);
 });
