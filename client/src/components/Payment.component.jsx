f7625dd7db16cddd3ec44a8af1066853d3f9d6f9
efsofian <ef.sofian@gmail.com>
2022-01-08 18:57:28 +0100

    test

--- client/src/components/Payment.component.jsx
+++ client/src/components/Payment.component.jsx
@@ -7,8 +7,8 @@ const Payment = () => {
 	const dispatch = useDispatch();
 	const stripeKey =
 		process.env.NODE_ENV === "production"
-			? process.env.REACT_APP_STRIPE_KEY
-			: process.env.stripePublishableKey;
+			? process.env.stripePublishableKey
+			: process.env.REACT_APP_STRIPE_KEY;
 	return (
 		<StripeCheckout
 			name={"Emailyx"}
