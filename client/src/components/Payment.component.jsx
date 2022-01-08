4458d676c081e86120b7fe860c1d2346a57dc6cf
efsofian <ef.sofian@gmail.com>
2022-01-08 18:44:07 +0100

    modif api keys

--- client/src/components/Payment.component.jsx
+++ client/src/components/Payment.component.jsx
@@ -5,13 +5,17 @@ import { handleToken } from "../redux/auth/auth.actions";
 
 const Payment = () => {
 	const dispatch = useDispatch();
+	const stripeKey =
+		process.env.NODE_ENV === "production"
+			? process.env.stripePublishableKey
+			: process.env.REACT_APP_STRIPE_KEY;
 	return (
 		<StripeCheckout
 			name={"Emailyx"}
 			description={"5$ for 5 email credits"}
 			amount={500}
 			token={(token) => dispatch(handleToken(token))}
-			stripeKey={process.env.REACT_APP_STRIPE_KEY}>
+			stripeKey={stripeKey}>
 			<button className="btn">Add Credits</button>
 		</StripeCheckout>
 	);
